8971a6bf5c423ccb443123da22d5d4481274891b
Leo <leoniro@gmail.com>
2023-09-28 19:24:53 -0300

    major restructuring

--- battleship/grid.py
+++
@@ -1,7 +0,0 @@
-from ship import Ship
-
-class Grid:
-    def __init__(self, height, width):
-        self.__height = height
-        self.__width = width
-        self.__ships = []
--- battleship/player.py
+++
@@ -1,4 +0,0 @@
-class Player:
-    def __init__(self, name):
-        self.__name = name
-        self.__score = 0
